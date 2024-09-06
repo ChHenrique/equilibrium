eb13be01c1ad643f8959687b776fd86e4521a3e1
ClaudHnrq <ch1claudiohenrique@gmail.com>
2024-09-06 11:36:09 -0300

    Verificação dos campos de login

--- frontend/src/pages/Logins/Login.pc/recognition/verification.jsx
+++ frontend/src/pages/Logins/Login.pc/recognition/verification.jsx
@@ -1,38 +1,15 @@
-import axios from 'axios';
 import { oversight } from './oversight';
 
-export async function Verification(e, { formRef, setErrors, setLoggedIn }) {
+export async function Verification(e, { formRef, setErrors}) {
     e.preventDefault();
-    
+
     // Valida o formulário
     const validationErrors = oversight(formRef);
     setErrors(validationErrors);
 
     // Se houver erros de validação, não envie a solicitação
-    if (Object.keys(validationErrors).length > 0) {
-        return;
-    }
-
-    // Obtém os dados do formulário
-    const form = formRef.current;  // Correção aqui
-    const user_email = form.querySelector('input[id="user_email"]').value.trim();
-    const password = form.querySelector('input[id="senha"]').value.trim();
-
-    try {
-        // Envia os dados para o backend
-        const response = await axios.post('http://localhost:3000/login_pc', {
-            email: user_email,
-            password: password,
-        });
-
-        // Processa a resposta da API
-        const { token } = response.data;
-        localStorage.setItem('token', token); // Armazena o token no localStorage
-        setLoggedIn = true ; // Atualiza o estado para refletir que o usuário está logado
-        console.log('Login bem-sucedido:', response.data);
-
-    } catch (error) {
-        console.error('Erro ao fazer login:', error.response ? error.response.data : error.message);
-        setErrors({ apiError: 'Erro ao fazer login, por favor tente novamente.' });
+    if (Object.keys(validationErrors).length === 0) {
+        console.log("enviado")
+        return
     }
 }
