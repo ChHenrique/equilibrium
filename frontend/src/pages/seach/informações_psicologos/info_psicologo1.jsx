af6d9cedac9e0ddd47f9635cb5515aa3282d7e7a
artizin9 <reis08897@gmail.com>
2024-09-19 13:21:55 -0300

    componetizando

--- frontend/src/pages/seach/informações_psicologos/info_psicologo1.jsx
+++ frontend/src/pages/seach/informações_psicologos/info_psicologo1.jsx
@@ -1,12 +1,12 @@
 import Dhamyla_psicologa from "../../../assets/images/dhamyla_ivina.png";
 
-export function Dhamyla_ivina() {
+export function Dhamyla_ivina(nome,foto,tempConsulta) {
     return(
         <div className="w-[75vw] h-auto bg-[#F1F5F9] rounded-2xl flex flex-col md:flex-row items-center p-4 md:p-6">
 
                 <div className="h-32 w-32 md:h-40 md:w-40 ml-0 md:-ml-2 mb-4 md:mb-0">
                     <img
-                        src={Dhamyla_psicologa}
+                        src={foto}
                         className="w-full h-full object-cover rounded-full"
                         alt="imagem_psicologo"
                     />
@@ -17,7 +17,7 @@ export function Dhamyla_ivina() {
                     <p className="text-xs md:text-sm font-poppins font-bold text-[#355081]">Nome</p>
 
                     <h1 className="text-[#355081] font-poppins font-semibold text-xl md:text-2xl underline">
-                        Psi. Dhamyla Ivina
+                        Psi. {nome}
                     </h1>
 
                     <h3 className="mt-1 text-xs md:text-sm font-satoshi font-medium text-[#355081]">
@@ -60,7 +60,7 @@ export function Dhamyla_ivina() {
 
                     <div className="w-full max-w-xs md:w-[6vw] h-[4vh] bg-[#E0F3E4] mt-4 rounded-md flex flex-col">
                         <h2 className="text-[#355081] text-xs font-satoshi font-semibold ml-1">Duração</h2>
-                        <h3 className="text-[#355081] text-xs font-satoshi font-extrabold underline ml-1">50 Minutos</h3>
+                        <h3 className="text-[#355081] text-xs font-satoshi font-extrabold underline ml-1">{tempConsulta}</h3>
                     </div>
                 </div>
 
