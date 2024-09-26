1c21c85e42459a1b7b7ef1ce83be0b4270a0fa4a
artizin9 <reis08897@gmail.com>
2024-09-26 18:13:11 -0300

    Update Header-seach.jsx

--- frontend/src/components/Header-seach.jsx
+++ frontend/src/components/Header-seach.jsx
@@ -1,14 +1,14 @@
 import { HeaderLink } from './header-link'
 import { Logo } from './logo'
-
+import  User_null from '../assets/images/user_null.svg'
 
 const LINKS = [
   { name: 'Sobre nós', href: '/sobre'},
-  { name: 'Procurar Psicólogos', href: '#' },
-  {name: 'Acessar consultas', href: '/consulta_pc'}
+  { name: 'Procurar Psicólogos', href: '/search' },
+  {name: 'Acessar consultas', href: '/homepage'}
 ]
 
-export function Header_seach({foto,nome}) {
+export function Header_seach() {
   return (
     <div
       id="Header"
@@ -24,8 +24,8 @@ export function Header_seach({foto,nome}) {
         ))}
         <div className='h-20 w-2/12 justify-end items-center flex m-4'>
 
-               <h1 className='font-poppins text-base font-medium text-slate-600 hover:text-hover hover:font-semibold transition ease min-w-fit duration-200 cursor-pointer mr-7'>{nome}</h1>
-               <img src={foto} alt="Foto de Perfil" className='h-8'/>
+               <h1 className='font-poppins text-base font-medium text-slate-600 hover:text-hover hover:font-semibold transition ease min-w-fit duration-200 cursor-pointer mr-7'>Pedro</h1>
+               <img src={User_null} alt="Foto de Perfil" className='h-8'/>
 
         </div>
       </div>
