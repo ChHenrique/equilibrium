b82cc25654e6b25b84f5d0339b3fa370fd569a80
ClaudHnrq <ch1claudiohenrique@gmail.com>
2024-09-29 18:21:02 -0300

    commit

--- frontend/src/pages/Dashboard/principal.jsx
+++ frontend/src/pages/Dashboard/principal.jsx
@@ -4,15 +4,16 @@ import { Camp_Dash } from "./sections/Center.jsx";
 import { Footer } from "../../components/footer.jsx";
 
 
-export function Dashboard(){
-    return(
-<div className="w-screen h-[120vh] bg-primary-300 justify-center flex items-center flex-col font-poppins overflow-x-hidden ">
-    <div className="mt-4 w-screen h-fit flex justify-center items-center ">
- <HeaderDash/>
- </div>
- <Camp_Dash/>
+export function Dashboard() {
+    return (
+        
+        <div className="w-screen h-[120vh] bg-primary-300 justify-center flex items-center flex-col font-poppins overflow-x-hidden ">
+            <div className="mt-4 w-screen h-fit flex justify-center items-center ">
+                <HeaderDash />
+            </div>
+            <Camp_Dash />
 
-<Footer/>
-</div>
+            <Footer />
+        </div>
     )
 }
--- frontend/src/pages/Dashboard/sections/Chats.jsx
+++ frontend/src/pages/Dashboard/sections/Chats.jsx
@@ -1,73 +1,82 @@
-import React from "react";
-import { useState } from "react";
+import React, { useRef, useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
 
 export function Chats() {
-    const [mensagens, setmensagens] = useState([])
-    const [value, setvalue] = useState('')
+    const [mensagens, setmensagens] = useState([]);
+    const [value, setvalue] = useState('');
+    const socketRef = useRef(null);
+
+    useEffect(() => {
+        //conexao como oservidor
+        socketRef.current = io.connect('http://localhost:3001');
+
+        //recebe de volta a mensagem do back
+        socketRef.current.on('receive_message', (message) => {
+            setmensagens(prevMensagens => [...prevMensagens, { text: message.text, authorId: message.authorId }]);
+        });
+
+
+        return () => {
+            socketRef.current.disconnect();
+        };
+    }, []);
 
     function pegavalor(e) {
-        setvalue(e.target.value)
+        setvalue(e.target.value);
     }
+
+    //envia a mensagem pro back pela funcao
     function aplicaMensagens() {
         if (value !== '') {
-            setmensagens([...mensagens, value])
-            setvalue('')
+
+            socketRef.current.emit('message', value);
+            setvalue('');
+            //limpa o input dps de enviar
         }
     }
 
-
-
     return (
-
         <div className="bg-white h-full max-h-full w-full font-satoshi-medium flex flex-row rounded-2xl">
-            {/*Aba dos conversantes*/}
-            <div className="h-full w-4/12 bg-slate-100 flex flex-col  rounded-l-2xl border-r-2 border-slate-400">
+            {/* Aba dos conversantes */}
+            <div className="h-full w-4/12 bg-slate-100 flex flex-col rounded-l-2xl border-r-2 border-slate-400">
                 <h1 className="text-2xl m-6 text-primary-700 font-medium">Chats</h1>
-                <div className="w-full h-2/12 bg-slate-100 justify-start items-end flex  rounded-tl-2xl border-b-2 border-slate-400">
-                    <input type="text" placeholder=" Pesquisar..." className=" w-11/12 m-4 h-9 bg-gray-200 rounded-xl placeholder:text-primary-700 outiline-2 outline-slate-400" />
+                <div className="w-full h-2/12 bg-slate-100 justify-start items-end flex rounded-tl-2xl border-b-2 border-slate-400">
+                    <input type="text" placeholder=" Pesquisar..." className="w-11/12 m-4 h-9 bg-gray-200 rounded-xl placeholder:text-primary-700 outline-2 outline-slate-400" />
                 </div>
-
-
-                
             </div>
 
-            {/*Aba do chat*/}
-            <div className="bg-white h-full  w-8/12 flex-col flex justify-start items-center rounded-r-2xl overflow-hidden m-0" >
-
-
-                {/*Chat*/}
-                <div id="Text" className="h-3/4 w-full bg-white flex flex-col items-end overflow-y-scroll overflow-x-hidden max-h-screen" >
-                    {mensagens.map((mensagem, index) => {
-                        return (
-                            <div className="w-full h-fit flex items-end">
-                                <div className="w-fit ml-auto h-fit whitespace-wrap overflow-wrap ">
-                                <h1 className="text-base bg-[#8095AB] max-w-2xl ml-auto p-2 m-2 h-max rounded-t-3xl rounded-bl-[24px] text-left break-all text-[#f0f0f0]" key={index}>{mensagem}</h1>
-                                </div>
+            {/* Aba do chat */}
+            <div className="bg-white h-full w-8/12 flex-col flex justify-start items-center rounded-r-2xl overflow-hidden m-0">
+                {/* Chat */}
+                <div id="Text" className="h-3/4 w-full bg-white flex flex-col items-end overflow-y-scroll overflow-x-hidden max-h-screen">
+                    {mensagens.map((mensagem, index) => (
+                        <div className={`h-fit flex items-end ${mensagem.authorId === socketRef.current.id ? 'ml-auto' : 'mr-auto'}`} key={index}>
+                            <div className="w-fit h-fit whitespace-wrap overflow-wrap">
+                                <h1 className={`text-base ${mensagem.authorId === socketRef.current.id ? 'bg-[#8095AB] rounded-bl-[24px]' : 'bg-[#A3D1D1] rounded-br-[24px]'} rounded-t-[24px] max-w-2xl p-2 m-2 h-max text-left break-all text-[#f0f0f0]`}>
+                                    {mensagem.text}
+                                </h1>
                             </div>
-                        )
-                    })}
+                        </div>
+                    ))}
+
+
+
 
 
                 </div>
 
-                
                 <div className="w-full justify-center items-center flex h-1/4 bg-white mt-auto m-8">
+                    <input type="text" className="w-9/12 h-12 rounded-2xl bg-secondary-100 mr-2 placeholder:text-primary-700 placeholder:p-2 border-2 border-slate-300 outline-1 outline-slate-400 p-2 m-8" placeholder="Mensagem" value={value} onChange={pegavalor} />
 
-                    <input type="text" className="w-9/12 h-12 rounded-2xl  bg-secondary-100 mr-2 placeholder: text-primary-700 placeholder:p-2 border-2 border-slate-300 outline-1  outline-slate-400 p-2 m-8 " placeholder="Mensagem" value={value} onChange={pegavalor} />
-
-                    {/*Botao de enviar do chat*/}
-
-                    <button className="h-12 w-12 rounded-2xl text-primary-700 hover:text-primary-500 transition duration-300 " onClick={aplicaMensagens}>
+                    {/* Botão de enviar do chat */}
+                    <button className="h-12 w-12 rounded-2xl text-primary-700 hover:text-primary-500 transition duration-300" onClick={aplicaMensagens}>
                         <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg" className="h-9 w-9">
-                            <path d="M45.8337 4.16663L22.917 27.0833M45.8337 4.16663L31.2503 45.8333L22.917 27.0833M45.8337 4.16663L4.16699 18.75L22.917 27.0833" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M45.8337 4.16663L22.917 27.0833M45.8337 4.16663L31.2503 45.8333L22.917 27.0833M45.8337 4.16663L4.16699 18.75L22.917 27.0833" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                     </button>
                 </div>
             </div>
-            <div>
-
-            </div>
-
         </div>
-    )
+    );
 }
