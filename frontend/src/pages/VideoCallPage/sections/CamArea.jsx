fe17db9d33e13072013acaac64547e3df70a5991
ClaudHnrq <ch1claudiohenrique@gmail.com>
2024-10-14 11:36:15 -0300

    coisnhas na camera

--- frontend/src/pages/VideoCallPage/sections/CamArea.jsx
+++ frontend/src/pages/VideoCallPage/sections/CamArea.jsx
@@ -12,22 +12,30 @@ const ImgdaWebcam = () => {
 
 
     return(
-      <div className="flex h-full w-full">
+      <div className="flex h-full w-full flex-col ">
         <div className="h-full w-full rounded-[20px] flex justify-center">
-               <div className="h-fit w-fit rounded-[20px]">
+               <div className="h-fit w-fit rounded-[20px] flex flex-col justify-between items-center">
                 {/*Webcam do usuario*/}
                  <Webcam ref={webRef}                        
                           className="rounded-[20px] w-[80%] mb-2"
                         videoConstraints={{ width: 1920, height: 1080, facingMode: "user" }} />
 
                         {/*Webcam do outro */}
-                 <div id="ex" className="rounded-[20px] w-[80%] h-[384px] bg-slate-800">
+                 <div id="ex" className="rounded-[20px] w-[80%] h- bg-slate-800"
+                 style={{
+                  aspectRatio: "16 / 9", 
+              }}
+                 >
 
 
 
                  </div>
               </div>
        </div>
+               {/*Botoes */}
+            <div className="w-full h-fit">
+               <button className=""></button>
+            </div>
       </div>
 
     )
