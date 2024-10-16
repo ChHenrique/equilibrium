5541b89e5706b49559d77237fc7605d8242a05d7
artizin9 <reis08897@gmail.com>
2024-10-16 12:35:58 -0300

    lala

---
+++ frontend/src/pages/informations/informations.psi/main/sections/id2_infopsi/informações_pessoais.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import "../../../infomations.pc/mid/style.css"
+
+export function Alterações() {
+
+   
+
+    return (
+        // Div do componente das alterções
+        <div className="w-full h-full flex items-center font-poppins font-medium relative rounded-tl-2xl">
+          
+        </div>
+    )
+}
