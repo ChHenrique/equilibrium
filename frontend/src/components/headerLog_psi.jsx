fdb5343a1b811dafb7f56c9a863c839b7ed7cf54
ClaudHnrq <ch1claudiohenrique@gmail.com>
2024-10-24 12:54:26 -0300

    CONSERTAR O HEADERLOGPSI DO FRONTEND

--- frontend/src/components/headerLog_psi.jsx
+++ frontend/src/components/headerLog_psi.jsx
@@ -10,7 +10,7 @@ const LINKS = [
 ];
 
 const LINKSUser = [
-    { name: 'Informações', href: '/info_pc' ,
+    { name: 'Informações', href: '/info_psi' ,
 
         svg: <svg width="32" height="32" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" className='m-1'>
         <path d="M40 42V38C40 35.8783 39.1571 33.8434 37.6569 32.3431C36.1566 30.8429 34.1217 30 32 30H16C13.8783 30 11.8434 30.8429 10.3431 32.3431C8.84285 33.8434 8 35.8783 8 38V42M32 14C32 18.4183 28.4183 22 24 22C19.5817 22 16 18.4183 16 14C16 9.58172 19.5817 6 24 6C28.4183 6 32 9.58172 32 14Z" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
