436ae4af23c98487fbdbcfd0337d43396f037f0b
artizin9 <reis08897@gmail.com>
2024-10-31 13:54:09 -0300

    esqueciminha senha iniciado

---
+++ frontend/src/pages/esqueci_senha/bolas.jsx
@@ -0,0 +1,16 @@
+
+export function Bolas() {
+  return (
+  <div>
+      
+  <div className="absolute top-[0] right-[0] w-[100px] h-[100px] bg-[#465A7F] rounded-bola1"></div>
+
+  <div className="absolute w-[100px] h-[100px] bg-[#465A7F] left-[0px] bottom-[0px] rounded-bola2"></div>
+
+  <div className="w-[44px] h-[90px] bg-[#8cb3ff] absolute right-[0px] top-[55px] rounded-meia_bola1"></div>
+
+  <div className="w-[44px] h-[90px] bg-[#8cb3ff] absolute left-[0px] bottom-[55px] rounded-meia_bola2"></div>
+
+  </div>
+  )
+}
--- frontend/src/pages/esqueci_senha/esqueci_senha.jsx
+++ frontend/src/pages/esqueci_senha/esqueci_senha.jsx
@@ -2,14 +2,21 @@ import linha1 from "../../assets/images/decoraçãoLoginRegistro1.svg";
 import linha2 from "../../assets/images/decoraçãoLoginRegistro2.svg";
 import linha3 from "../../assets/images/decoraçãoLoginRegistro3.svg";
 import "../../log,reg.css"
+import { Bolas } from "./bolas";
 
 export function EsqueciSenha() {
   return(
-  <div className = 'bg-primary-300 flex-col flex justify-center items-center w-full h-[100vh]  overflow-hidden'>
-
+  <div className = 'bg-primary-300 flex-col flex justify-center items-center w-full h-[100vh] overflow-hidden'>
             <img id='l1' src={linha1} alt="" className='absolute right-0 top-0'/>
             <img id='l2' src={linha2} alt="" className='absolute right-0 -top-20'/>
-            <img id='l3' src={linha3} alt="" className='absolute right-0 -top-20'/>    
+            <img id='l3' src={linha3} alt="" className='absolute right-0 -top-20'/>  
+
+            <div className = "bg-slate-50 w-[30%] aspect-square flex flex-col p-12 shadow-2xl rounded-md space-y-16 relative">
+              <Bolas/>
+              <h1 className="font-poppins text-[30px] text-primary-700 mt-16">Precisamos verificar sua <span>indentidade</span></h1>
+
+              <h1>pepe</h1>
+            </div>  
         </div>
   )
 }
