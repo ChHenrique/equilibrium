76b52877d0c00b2ff3cb94172345c76d8fe2af32
ricardokks <161058381+ricardokks@users.noreply.github.com>
2024-11-01 14:58:10 -0300

    Correção na camera e estilização da propria
    Ajeitei a camera porque o peerref nao estava sendo usado corretamente, porque toda vez que voce ia usar a camera ele era renderizado antes de usar mas nao enquanto usava porque a logica de  uso do peer stream tava errada no <video/>
    
    tambem ajeitei a função de usar online o chat e a webcam usando o --host e --port

--- frontend/src/pages/VideoCallPage/sections/CamArea.jsx
+++ frontend/src/pages/VideoCallPage/sections/CamArea.jsx
@@ -6,6 +6,10 @@ import Webcam from 'react-webcam';
 const socket = io('http://localhost:5000');
 
 export function VideoChat() {
+    const [renderUser, setRenderuser] = useState(1)
+
+
+   
     const webcamRef = useRef(null);
     const peerRef = useRef(null);
     const [peerStream, setPeerStream] = useState(null);
@@ -32,49 +36,118 @@ export function VideoChat() {
         };
 
         if (webcamRef.current && webcamRef.current.video) {
-            navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(handleUserMedia);
+            navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(handleUserMedia).catch(error => {
+                console.error("erro ao tentar acessar microfone e camera", error);
+                alert("nao da pra acessar microfone e camera");
+            });
         }
 
         return () => {
-            if (peerRef.current) peerRef.current.destroy();
+            socket.off('signal');
+            if (peerRef.current) {peerRef.current.destroy();
+            peerRef.current = null;}
         };
     }, []);
 
-    return (
-        <div>
-            <Webcam
-                audio={true}
-                ref={webcamRef}
-                videoConstraints={{ width: 1280, height: 720, facingMode: 'user' }}
-            />
-            <div>
-                <h2>Sua câmera:</h2>
+    useEffect(() => {
+        if (peerStream && videoRef.current) {
+            videoRef.current.srcObject = peerStream;
+        }
+    }, [peerStream]);
+
+    const videoRef = useRef(null)
+
+
+    function RenderUserCam(){
+        if(renderUser == 1){
+            return(
+            <div className='h-fit w-full relative mb-4 mt-4'>
+                <Webcam
+                    className="rounded-[20px] w-full  bg-slate-800 max-w-[80%] flex justify-center items-center"
+                    style={{aspectRatio: "16 / 9",}}
+                    audio={true}
+                    ref={webcamRef}
+                    videoConstraints={{ width: 1280, height: 720, facingMode: 'user' }}
+                />
+                <h1 className='absolute bottom-[5%] left-4 text-white font-poppins font-normal'>Você:</h1>
+            </div>)
+        }else{
+            return(
+            <div className='h-fit w-full relative mb-4 mt-4'>
+                <div id="ex" className="rounded-[20px] w-full  bg-slate-800 max-w-[80%] flex justify-center items-center"
+                    style={{
+                        aspectRatio: "16 / 9",
+                    }}
+                >
+
+                    <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_865_941)">
+                            <path d="M46 14L32 24L46 34V14Z" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                            <path d="M28 10H6C3.79086 10 2 11.7909 2 14V34C2 36.2091 3.79086 38 6 38H28C30.2091 38 32 36.2091 32 34V14C32 11.7909 30.2091 10 28 10Z" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                            <line x1="1.58579" y1="46.5858" x2="46.5858" y2="1.58579" stroke="#FFFFFF" stroke-width="4" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_865_941">
+                                <rect width="48" height="48" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </div>
+
+            </div>)
+        }
+    }
+    function RenderOtherCam(){
+        if(peerStream != null){
+            return(
+            <div className='h-fit w-full relative mb-4 mt-4'>
                 <video
+                className=' rounded-[20px] w-full  bg-slate-800 max-w-[80%] flex justify-center items-center aspect-video'
                     playsInline
                     autoPlay
-                    ref={(ref) => {
-                        if (ref && webcamRef.current && webcamRef.current.video) {
-                            ref.srcObject = webcamRef.current.video.srcObject;
-                        }
-                    }}
-                    width={400}
-                    height={300}
+                    ref={videoRef}
                 />
-            </div>
-            {peerStream && (
-                <div>
-                    <h2>Câmera remota:</h2>
-                    <video
-                        playsInline
-                        autoPlay
-                        ref={(ref) => {
-                            if (ref) ref.srcObject = peerStream;
+                <h1 className='absolute bottom-[5%] left-4 text-white font-poppins font-normal'>Outro:</h1>
+            </div>)
+        }else{
+            
+                return(
+                <div className='h-fit w-full relative mb-4 mt-4'>
+                    <div id="ex" className="rounded-[20px] w-full  bg-slate-800 max-w-[80%] flex justify-center items-center"
+                        style={{
+                            aspectRatio: "16 / 9",
                         }}
-                        width={400}
-                        height={300}
-                    />
-                </div>
-            )}
+                    >
+    
+                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_865_941)">
+                                <path d="M46 14L32 24L46 34V14Z" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M28 10H6C3.79086 10 2 11.7909 2 14V34C2 36.2091 3.79086 38 6 38H28C30.2091 38 32 36.2091 32 34V14C32 11.7909 30.2091 10 28 10Z" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                                <line x1="1.58579" y1="46.5858" x2="46.5858" y2="1.58579" stroke="#FFFFFF" stroke-width="4" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_865_941">
+                                    <rect width="48" height="48" fill="white" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
+                    <h1 className='absolute bottom-[5%] left-4 text-white font-poppins font-normal'>Outro:</h1>
+    
+                </div>)
+            }
+
+        }
+
+    
+
+    return (
+        <div className='h-full w-full flex justify-center items-center flex-col'>
+
+
+            {RenderUserCam()}
+
+            {RenderOtherCam()}
         </div>
     );
 }
