41798d309941636edfb16e4d044c5670c80456f2
ClaudHnrq <ch1claudiohenrique@gmail.com>
2024-11-04 16:38:52 -0300

    adiçao dos botoes e coreçção de resposividade cam area

--- frontend/src/pages/VideoCallPage/principal.jsx
+++ frontend/src/pages/VideoCallPage/principal.jsx
@@ -7,7 +7,7 @@ window.global = window; // Adicionando o polyfill diretamente
 export function VideoPage() {
   return (
     <div className="bg-primary-300 w-full h-[140vh] flex flex-col justify-between items-center">
-      <div className="w-5/6 h-4/6 mt-10">
+      <div className="w-5/6 h-4/6 mb-[10%] mt-10 max-md:mt-2">
         <Center />
       </div>
       <Footer />
