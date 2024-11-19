129c09319107da11fdd9f6253f30d223ec9e9615
artizin9 <reis08897@gmail.com>
2024-11-19 12:44:48 -0300

    isso ai

--- frontend/src/pages/registration/registration.pc/sections/container.jsx
+++ frontend/src/pages/registration/registration.pc/sections/container.jsx
@@ -1,15 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Header } from './header';
 import { Balls } from '../storage/balls';
 import { Form } from './form';
 import logo from "../../../../assets/images/logo-title.svg";
+import "../registro_pc.css"
 
 export function Container() {
     const formRef = useRef(null)
     const [errors, setErrors] = React.useState({ general: '' })
 
     return (
-        <div className='bg-white flex-col flex items-center p-0 h-[85vh] w-[65vw] relative rounded-[10px] max-sm:h-[90vh] max-sm:w-[85vw]'>
+        <div id = "conteiner" className= "bg-white flex-col flex items-center p-0 h-[85vh] w-[65vw] relative rounded-[10px] max-sm:h-[90vh] max-sm:w-[85vw]">
             <Header />
             <Form ref={formRef} errors={errors} setErrors={setErrors}/>
             <Balls />
