2abb3bae288dc1102dc6c0d3f0aca4766eb9f8a0
ClaudHnrq <ch1claudiohenrique@gmail.com>
2024-11-28 12:26:22 -0300

    Adição da equipe

--- frontend/src/pages/about/sections/participants.jsx
+++ frontend/src/pages/about/sections/participants.jsx
@@ -8,6 +8,11 @@ import dhamyla from '../../../assets/images/participants/Dhamyla.jpeg'
 import ricardo from '../../../assets/images/participants/ricardo.jpeg';
 import claudio from '../../../assets/images/participants/claudio.jpeg';
 import pedro from '../../../assets/images/participants/pedro.jpg';
+import stefany from '../../../assets/images/participants/Stefany.jpeg'
+import hellen from '../../../assets/images/participants/hellen.jpeg'
+import isabele from '../../../assets/images/participants/isabele.jpeg'
+import juan from '../../../assets/images/participants/juan.jpeg'
+
 import { ParticipantsCard } from '../../../components/participant-card';
 
 export function Participants() {
@@ -35,7 +40,27 @@ export function Participants() {
       name: 'Pedro Henrique',
       imageUrl: pedro,
       description: 'Me chamo Pedro Henrique, sou um estudante de Desenvolvimento de Sistemas e Front-End Developer na startup Equilibrium. Com habilidades avançadas em HTML, CSS, JavaScript e React, ele se especializa na criação e otimização de interfaces web para oferecer experiências de usuário eficientes e intuitivas. Pedro é comprometido com a excelência e a inovação, sempre buscando aprimorar suas competências e contribuir para o sucesso dos projetos.',
-    },
+    },{
+      id:3,
+      name: "Estefany Ágata",
+      imageUrl: stefany,
+      description:"Meu nome é Estefany Ágata, tenho 17 anos e sou designer. Atualmente, estudo na Escola Francisca Nelita, onde estou no 2º ano do ensino médio. Estou fazendo um curso de desenvolvimento de sistemas, que complementa minha paixão pelo design. Adoro criar e explorar novas ideias visuais, e me considero boa nisso! Estou sempre em busca de aprender mais e aprimorar minhas habilidades para melhorar tornar uma profissional ainda melhor na área."
+    },{
+      id:4,
+      name: "Hellen Kethili",
+      imageUrl: hellen,
+      description: " Sou a Hellen Kethili, tenho 17 anos , sou estudante do curso 2° Desenvolvimento de Sistemas e atuo como designer da startup Equilíbrium.Sou uma pessoa bastante criativa , Tenho uma abordagem criativa Para resolver problemas  sempre buscando soluções inovadoras e originais;me considero uma pessoa bastante curiosa, pois busco explorar novas técnicas , estilos e abordagens no design e sou muito detalhista também , prezo pela precisão e atenção nos detalhes em todos os aspectos. "
+    },{
+      id:5,
+      name: "Isabelle Brito",
+      imageUrl: isabele,
+      description: "Meu nome é Isabelle Brito, tenho 16 anos e sou designer. Estou no 2º ano do ensino médio e faço um curso de desenvolvimento de sistemas, que complementa minha paixão pelo design. Adoro criar e explorar novas ideias visuais, e estou sempre buscando aprender mais e aprimorar minhas habilidades para me tornar uma profissional ainda melhor na área"
+    },{
+      id: 6,
+      name: "Juan Carlos",
+      imageUrl : juan,
+      description : "Meu nome é Juan Carlos, tenho 16 anos e sou desenvolvedor Frontend. Estou no 2º ano do ensino médio e tenho paixão por criar interfaces interativas e dinâmicas. Sempre me atualizo sobre as tecnologias modernas para melhorar minhas habilidades no desenvolvimento Frontend e oferecer experiências incríveis aos usuários."
+    }
 
   ]
   return (
