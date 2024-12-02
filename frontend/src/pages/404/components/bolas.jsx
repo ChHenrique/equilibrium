3970f3928ce9144851a08378cc40aab617e783e9
ricardokks <161058381+ricardokks@users.noreply.github.com>
2024-12-02 15:27:20 -0300

    .

--- frontend/src/pages/404/components/bolas.jsx
+++ frontend/src/pages/404/components/bolas.jsx
@@ -4,9 +4,9 @@ export function Balls() {
     return (
     <div>
         
-        <div className="absolute top-[0] right-[0] w-[200px] h-[200px] bg-[#465A7F] rounded-bl-[120px] rounded-tr-[16px] max-md:w-[60px] max-md:h-[60px] max-lg:w-[100px] max-lg:h-[100px] max-xl:w-[100px] max-xl:h-[100px] max-2xl:h-[130px] max-2xl:w-[130px]"></div>
+        <div className="absolute top-[0] right-[0] w-[200px] h-[200px] bg-[#465A7F] rounded-bl-[200px] rounded-tr-[16px] max-md:w-[60px] max-md:h-[60px] max-lg:w-[100px] max-lg:h-[100px] max-xl:w-[100px] max-xl:h-[100px] max-2xl:h-[130px] max-2xl:w-[130px]"></div>
     
-        <div className="absolute w-[200px] h-[200px] bg-[#465A7F] left-[0px] bottom-[0px] rounded-tr-[120px] rounded-bl-[16px] max-md:w-[60px] max-md:h-[60px] max-lg:w-[100px] max-lg:h-[100px] max-xl:w-[100px] max-xl:h-[100px] max-2xl:h-[130px] max-2xl:w-[130px]"></div>
+        <div className="absolute w-[200px] h-[200px] bg-[#465A7F] left-[0px] bottom-[0px] rounded-tr-[200px] rounded-bl-[16px] max-md:w-[60px] max-md:h-[60px] max-lg:w-[100px] max-lg:h-[100px] max-xl:w-[100px] max-xl:h-[100px] max-2xl:h-[130px] max-2xl:w-[130px]"></div>
     
         <div className="w-[50px] h-[105px] bg-[#8cb3ff] absolute right-[0px] top-[155px] rounded-meia_bola1 max-md:w-[25px] max-md:h-[51.14px] max-md:top-[45px] max-lg:top-[55px] max-xl:w-[40px] max-xl:h-[84px] max-xl:top-[70px] max-2xl:top-[95px]"></div>
     
